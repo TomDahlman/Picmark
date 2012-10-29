a40450fe505a5c0fcc7ecd1e3e2d2e7c89ccd3ad
Arjun Venkataswamy <avenkat2@gmail.com>
2012-10-29 09:08:14 -0500

    comments on code. There were just small issues, nice work, Tom.

--- app/controllers/picmarks_controller.rb
+++ app/controllers/picmarks_controller.rb
@@ -2,37 +2,37 @@ class PicmarksController < ApplicationController
   def index
     @pictures = Picture.all
   end
-  
+
   def show
     @pic  = Picture.find(params[:id])
   end
-  
+
   def new
   end
-  
+
   def create
-    p = Picture.new
+    p = Picture.new # this is fine, but remember that mass assignment is also an option
     p.url = params[:url]
     p.title = params[:title]
     p.save
-    redirect_to all_pictures
+    redirect_to all_pictures # don't forget to add the _url to the method name
   end
-  
+
   def edit
     @pic = Picture.find(params[:id])
   end
-  
+
   def update
     p = Picture.find(params[:id])
     p.url = params[:url]
     p.title = params[:title]
     p.save
-    redirect_to 'one_picture'
+    redirect_to 'one_picture' # make sure the method name is not in quotes, since it's not a string. So instead of 'one_picture', it should be one_picture_url(p.id), since the method needs the id of the object to get back to the show page
   end
-  
+
   def destroy
     Picture.destroy(params[:id])
-    redirect_to 'all_pictures'
+    redirect_to 'all_pictures' # same as line 30
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Picmarks::Application.routes.draw do
-  
-  get '/pictures' => 'picmarks#index', :as => :all_pictures
-  
-  
+
+  get '/pictures' => 'picmarks#index', :as => :all_pictures # try to use standard conventions for your named routes
+
+
   get '/pictures/new' => 'picmarks#new', :as => :new_picture
   post '/pictures' => 'picmarks#create'
-  
-  get '/pictures/:id' => 'picmarks#show', :as => :one_picture
-  
+
+  get '/pictures/:id' => 'picmarks#show', :as => :one_picture # try to use standard conventions for your named routes
+
   get '/pictures/edit/:id' => 'picmarks#edit', :as => :edit_picture
   put '/pictures/:id' => 'picmarks#update'
-  
+
   delete '/pictures' => 'picmarks#destroy'
-  
+
 end
